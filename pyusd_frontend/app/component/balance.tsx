6b6824cecc80a66e4eb130e6d80db5fc649f9bec
Dannynsikak <nsikakdanny11@gmail.com>
2025-03-07 17:55:54 +0100

    fix: moved the checkBal component to the head of the component

--- pyusd_frontend/app/component/balance.tsx
+++ pyusd_frontend/app/component/balance.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { useState } from "react";
 import { fetchBalance } from "../../webSocketHook";
+import Link from "next/link";
 
 export default function BalanceChecker() {
   const [address, setAddress] = useState("");
@@ -29,27 +30,35 @@ export default function BalanceChecker() {
   };
 
   return (
-    <div className="min-h-[20%] bg-[#13132A] text-white p-10 rounded-lg shadow-lg">
-      <h1 className="text-2xl font-bold mb-5">🏦 Check Wallet Balance</h1>
-      <input
-        type="text"
-        className="p-2 text-white"
-        placeholder="Enter Wallet Address"
-        value={address}
-        onChange={(e) => setAddress(e.target.value)}
-      />
-      <button
-        type="button"
-        className="bg-blue-500 p-2 ml-2 rounded-lg"
-        onClick={checkBalance}
-        disabled={loading}
-      >
-        {loading ? "Checking your balance....." : "Check"}
-      </button>
-      {error && <p className="mt-4 text-red-500">{error}</p>}
-      {balance && (
-        <p className="mt-4 text-green-600 font-bold">Balance: {balance}</p>
-      )}
+    <div className="min-h-[20px] flex items-center justify-between bg-[#13132A] text-white p-10 rounded-lg">
+      <div className="shadow-lg flex  gap-5 ">
+        <h1 className="text-2xl font-bold mb-5">🏦 Check Wallet Balance</h1>
+        <div className="items-center p-5">
+          <input
+            type="text"
+            className="p-2 text-white"
+            placeholder="Enter Wallet Address"
+            value={address}
+            onChange={(e) => setAddress(e.target.value)}
+          />
+          <button
+            type="button"
+            className="bg-blue-500 p-2 ml-2 rounded-lg"
+            onClick={checkBalance}
+            disabled={loading}
+          >
+            {loading ? "Checking your balance....." : "Check"}
+          </button>
+          {error && <p className="mt-4 text-red-500">{error}</p>}
+          {balance && (
+            <p className="mt-4 text-green-600 font-bold">Balance: {balance}</p>
+          )}
+        </div>
+      </div>
+      <div className="rounded-lg border-white border-2 p-1">
+        {" "}
+        <Link href={"https://www.coingecko.com/"}>Join the Fam</Link>
+      </div>
     </div>
   );
 }
